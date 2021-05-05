67bcc39956a3243198292df428bd5119d28de668
Ainne Oum <ainneo@gmail.com>
2021-05-05 01:06:21 -0700

    navbar version 1 non-scroll

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Projects from "./components/pages/Projects";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
+import Home from "./components/pages";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
+        {/* <Route path="/" exact component={Home} /> */}
         <Route path="/projects" exact component={Projects} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/about" exact component={About} />
