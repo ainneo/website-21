693e702375ef1af5253a0a3a06a0629cd100e0ed
Ainne Oum <ainneo@gmail.com>
2021-05-05 00:27:51 -0700

    navbar version 1 non-scroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Projects from "./components/pages/Projects";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/about" exact component={About} />
+      </Switch>
     </Router>
   );
 }
