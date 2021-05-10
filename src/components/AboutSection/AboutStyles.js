5bc08c12c423733b5d4011cd539cb453441c08be
Ainne Oum <ainneo@gmail.com>
2021-05-10 11:04:36 -0700

    update fav icon & opg tags

--- src/components/AboutSection/AboutStyles.js
+++ src/components/AboutSection/AboutStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AboutContainer = styled.div`
   width: 100%;
-  height: 90vh;
+  height: 80vh;
   display: flex;
   justify-content: center;
   align-items: center;
